c8f12eea06bd83bdda843c392b85d6b591c8dfb3
Ryan Lindgren <ryan.lindgren@gmail.com>
2014-11-02 22:06:15 -0500

    feat: add support for glob-matched step files and fix error when cucumber already installed

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 var es = require('event-stream');
 var spawn = require('child_process').spawn;
+var glob = require('glob');
+var fs = require('fs');
 
-var WinPath = process.cwd() + '\\node_modules\\.bin\\cucumber-js.cmd';
-var UnixPath = process.cwd() + '/node_modules/cucumber/bin/cucumber.js';
+var localCuc, binPath;
+if (process.platform === 'win32') {
+    localCuc = fs.existsSync('\\node_modules\\.bin\\cucumber-js.cmd');
+    binPath = localCuc ? '\\node_modules\\.bin\\cucumber-js.cmd' : __dirname + '\\node_modules\\.bin\\cucumber-js.cmd';
+} else {
+    localCuc = fs.existsSync('/node_modules/cucumber/bin/cucumber.js');
+    binPath = localCuc ? '/node_modules/cucumber/bin/cucumber.js' : __dirname + '/node_modules/cucumber/bin/cucumber.js';
+}
+
+var _once = function (fn, ctx) {
+    var count = 0;
+    return function () {
+        if (!count && count++) return fn.apply(ctx, Array.prototype.slice.call(arguments));
+    };
+};
+
 
 var cucumber = function(options) {
 
     var runOptions = [];
 
     if (options.steps) {
-        runOptions.push('-r');
-        runOptions.push(options.steps);
+        glob.sync(options.steps).forEach(function (file) {
+            runOptions.push('-r');
+            runOptions.push(file);
+        });
     }
 
-    if (process.platform === 'win32') {
-        binPath = WinPath;
-    } else {
-        binPath = UnixPath;
-    }
+    var writeOnce = _once(process.stdout.write, process.stdout);
 
     var run = function(argument, callback) {
         var filename = argument.path;
@@ -42,7 +56,8 @@ var cucumber = function(options) {
             var startIndex = data.substring(0, data.indexOf('"keyword": "Feature"')).lastIndexOf('[');
             var featureOutput = data.substring(startIndex);
             process.stdout.write(featureOutput.trim());
-            process.stdout.write('\r\nFeature: ' + filename);
+            writeOnce('\r\n');
+            process.stdout.write('Feature: ' + filename + '\r\n');
         });
         return callback();
     };
