0d58a1fad829f5253f3fa107b45d2a9f1f8ff42e
Volodymyr Gamula <v.gamula@gmail.com>
2015-01-19 22:47:53 +0200

    Added forgotten semicolons, removed unused variables

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var through2 = require('through2');
-var glob = require('simple-glob');
-var fs = require('fs');
 var Cucumber = require('cucumber');
+var glob = require('simple-glob');
+var through2 = require('through2');
 
 var cucumber = function(options) {
+    var files = [];
     var runOptions = [];
 
-    // load support files and step_definitions from options
-    var files = [];
     if (options.support) {
         files = files.concat(glob([].concat(options.support)));
     }
@@ -32,24 +30,25 @@ var cucumber = function(options) {
         if (filename.indexOf(".feature") === -1) {
             return callback();
         }
-
         features.push(filename);
         callback();
     };
 
     var run = function(callback) {
         var argv = ['node', 'cucumber-js'];
+        
         argv.push.apply(argv, runOptions);
         argv.push.apply(argv, features);
-        var stream = this
+
+        var stream = this;
         Cucumber.Cli(argv).run(function(succeeded) {
             if (succeeded) {
-                callback()
-                stream.emit('end')
+                callback();
+                stream.emit('end');
             } else {
-                callback(new Error("Cucumber tests failed!"))
+                callback(new Error("Cucumber tests failed!"));
             }
-        })
+        });
     };
 
     return through2.obj(collect, run);
