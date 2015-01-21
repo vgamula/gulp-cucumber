687f16096f61729d8b11f65e266b95dcd8b8ef00
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-01-21 17:14:36 +0700

    update test to check for stream end event

--- features/support/meow.js
+++ features/support/meow.js
@@ -1,8 +1,6 @@
 
 module.exports = function() {
     this.World.prototype.meow = function() {
-        for (var i = 0; i < 4; i ++) {
-            console.log(Array(16).join('meow '));
-        }
-    }
-}
+        console.log('meow!!');
+    };
+};
